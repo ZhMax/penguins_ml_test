b0315c0cf2b16ac3a25ce16788c1e1f9e3aa3536
Max Zhelnin <zhelninmax@gmail.com>
2022-10-01 20:46:26 +0500

    Edit Condition

--- penguins_streamlit.py
+++ penguins_streamlit.py
@@ -22,59 +22,59 @@ password_guess = st.text_input('What is the Password?')
 if password_guess != 'streamlit_is_great': 
   st.stop() 
 
-st.title('Illustrating the Central Limit Theorem with \
-    Streamlit')
+# st.title('Illustrating the Central Limit Theorem with \
+#     Streamlit')
 
-# penguin_file = st.file_uploader('Upload your own penguin data') 
+penguin_file = st.file_uploader('Upload your own penguin data') 
 
-# if penguin_file is None: 
-#     penguin_df = pd.read_csv('penguins.csv')
+if penguin_file is None: 
+    penguin_df = pd.read_csv('penguins.csv')
     
-#     rf_pickle = open('random_forest_penguin.pickle', 'rb') 
+    rf_pickle = open('random_forest_penguin.pickle', 'rb') 
 
-#     map_pickle = open('output_penguin.pickle', 'rb') 
+    map_pickle = open('output_penguin.pickle', 'rb') 
 
-#     rfc = pickle.load(rf_pickle) 
+    rfc = pickle.load(rf_pickle) 
 
-#     unique_penguin_mapping = pickle.load(map_pickle) 
+    unique_penguin_mapping = pickle.load(map_pickle) 
 
-#     rf_pickle.close() 
+    rf_pickle.close() 
 
-#     map_pickle.close() 
+    map_pickle.close() 
 
-# else: 
+else: 
 
-#     penguin_df = pd.read_csv(penguin_file) 
+    penguin_df = pd.read_csv(penguin_file) 
 
-#     penguin_df = penguin_df.dropna() 
+    penguin_df = penguin_df.dropna() 
 
-#     output = penguin_df['species'] 
+    output = penguin_df['species'] 
 
-#     features = penguin_df[['island', 'bill_length_mm', 'bill_depth_mm', 
+    features = penguin_df[['island', 'bill_length_mm', 'bill_depth_mm', 
 
-#                            'flipper_length_mm', 'body_mass_g', 'sex']] 
+                           'flipper_length_mm', 'body_mass_g', 'sex']] 
 
-#     features = pd.get_dummies(features) 
+    features = pd.get_dummies(features) 
 
-#     output, unique_penguin_mapping = pd.factorize(output) 
+    output, unique_penguin_mapping = pd.factorize(output) 
 
  
 
-#     x_train, x_test, y_train, y_test = train_test_split( 
+    x_train, x_test, y_train, y_test = train_test_split( 
 
-#         features, output, test_size=.8) 
+        features, output, test_size=.8) 
 
-#     rfc = RandomForestClassifier(random_state=15) 
+    rfc = RandomForestClassifier(random_state=15) 
 
-#     rfc.fit(x_train, y_train) 
+    rfc.fit(x_train, y_train) 
 
-#     y_pred = rfc.predict(x_test) 
+    y_pred = rfc.predict(x_test) 
 
-#     score = round(accuracy_score(y_pred, y_test), 2) 
+    score = round(accuracy_score(y_pred, y_test), 2) 
 
-#     st.write('We trained a Random Forest model on these data,' 
-#              ' it has a score of {}! Use the ' 
-#              'inputs below to try out the model.'.format(score))
+    st.write('We trained a Random Forest model on these data,' 
+             ' it has a score of {}! Use the ' 
+             'inputs below to try out the model.'.format(score))
 
 # with st.form('user_inputs'): 
 #   island = st.selectbox('Penguin Island', options=[
